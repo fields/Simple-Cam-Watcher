e3d488f6d4dbacaee44b22434c70b0f8299d15f1
Adam Fields <github345098@aquick.org>
2011-01-25 22:34:48 -0500

    added more comments on config files and a note about which twilio gem

--- config_sample.rb
+++ config_sample.rb
@@ -1,8 +1,15 @@
+# the path to watch on your computer
 $WATCHDIR = '/your/dropbox/fullpath/camwatcher/*'
+
+# your twilio credentials
 $MY_TWILIO_SID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 $MY_AUTH_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+# the phone number you want sms notifications to go to
 $TO_PHONE_NUM = '+1xxxxxxxxxx'
+# For trial twilio accounts, the from number must be your sandbox number.
 $FROM_PHONE_NUM = '+1xxxxxxxxxx'
+
+# default delays and loops
 $INITIAL_DELAY = ENV['INITIAL_DELAY'] || 300
 $WAIT_INTERVAL = ENV['WAIT_INTERVAL'] || 120
 $NOTIFY_ON_LOOP = ENV['NOTIFY_ON_LOOP'] || 5
